3e4f74d45237807ff4d82c8f4d25cb706da6d377
chesco <achesco@gmail.com>
2012-06-15 16:40:46 +0300

    Неймспейсы.

---
+++ js/f.js
@@ -0,0 +1,20 @@
+/**
+ * @namespace "Фундамент".
+ */
+var f = f || {};
+/**
+ * @namespace UI штучки.
+ */
+f.ui = f.ui || {};
+/**
+ * @namespace Формы.
+ */
+f.form = {};
+/**
+ * @namespace Вспомогательные булы.
+ */
+f.is = f.is || {};
+/**
+ * @namespace Утилиты.
+ */
+f.util = f.util || {};
